ca0445aa9013bc3f6a7d7214e9bba348f641b286
natpuot92 <natpuot1992@gmail.com>
2017-07-25 23:02:52 +0300

    first commit

---
+++ app/initialize.js
@@ -0,0 +1,44 @@
+import $ from "jquery"
+import fullpage from 'fullpage.js'
+import slick from 'slick-carousel'
+
+$(document).ready(function($) {
+  $('#fullpage').fullpage({
+    anchors:['page-1','page-2', 'page-3', 'page-4', 'page-5', 'page-6', 'page-7', 'page-8', 'page-9', 'page-10'],
+    menu:' #menu',
+    navigation: true,
+    navigationPosition: 'left'
+  });
+})
+
+function initSlick () {
+  $('.features-list').slick({
+    infinite: true,
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+    ]
+  })
+}
+
+$(document).ready(() => {
+  initSlick()
+})
