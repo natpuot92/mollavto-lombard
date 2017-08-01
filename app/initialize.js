9c0a92c5f641e4fa5a19928269b87e6f048c623d
vanilla-ice <valeraerohin97@mail.ru>
2017-08-01 21:09:22 +0300

    Fix: slider

--- app/initialize.js
+++ app/initialize.js
@@ -174,7 +174,7 @@ $('.section9__slider').on('beforeChange', function(event, slick, currentSlide, n
     slidesToShow: 1,
     slidesToScroll: 1,
     fade: true,
-    pauseOnHover: true,
+    variableWidth: true,
     appendArrows: $('.popup-slider__arrows'),
     prevArrow: '<button id="prev" type="button" class="btn section3-btn-prev popup-slider-prev"><span class = "section3-btn-prev-text popup__slider-prev-text">Назад</span><div class="section3-btn-prev-arrow"></div></button>',
     nextArrow: '<button id="next" type="button" class="btn section3-btn-next popup-slider-next"><span class = "section3-btn-next-text popup__slider-next-text">Вперед</span><div class="section3-btn-next-arrow"></div></button>'
@@ -239,7 +239,7 @@ var askElements = $('.section8__ask');
 
 askElements.each(function(i, element) {
   $(element).click(function(evt) {
-    evt.preventDefault();
+    //evt.preventDefault();
 
     var slideNumber = $(evt.target).attr('data-slide');
 
